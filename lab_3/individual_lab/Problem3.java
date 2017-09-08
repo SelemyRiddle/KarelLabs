3a6763ab77083e021567238c541323b1dad4da6b
SelemyRiddle <31352880+SelemyRiddle@users.noreply.github.com>
2017-09-08 09:40:08 -0700

    lab3Day2

--- lab_3/individual_lab/Problem3.java
+++ lab_3/individual_lab/Problem3.java
@@ -3,7 +3,7 @@ package lab_3.individual_lab;
 import kareltherobot.*;
 import java.awt.Color;
 
-public class Problem3 extends Robot
+public class Problem3 extends Problem2
 {
    
     /**
@@ -15,7 +15,111 @@ public class Problem3 extends Robot
     }
 
     public void carpetRooms(){
-        
+        move();
+        checkRoom();
+        turnLeft();
+        move();
+        checkRoom();
+        turnLeft();
+        move();
+        checkRoom();
+        turnLeft();
+        move();
+        checkRoom();
+        turnLeft();
+        move();
+        checkRoom();
+        turnLeft();
+        move();
+        checkRoom();
+        turnLeft();
+        move();
+        checkRoom();
+        turnLeft();
+        move();
+        checkRoom();
+    }
+    public void checkRoom(){
+        turnLeft();
+        move();
+        if (frontIsClear()==false){
+            if (checkWalls() == true){
+                putBeeper();
+                faceSouth();
+                leaveRoom();
+            }
+        }
+        else{
+            if (checkWalls() == true){
+               checkRoom();
+            }
+        }
+    }
+    public boolean checkWalls(){
+        turnLeft();
+        if (frontIsClear()==false){
+                turnLeft();
+                turnLeft();
+                if (frontIsClear()==false){
+                    return true;
+                }
+                else{
+                    notARoom();
+                    return false;
+                }
+        }
+            else{
+                notARoom();
+                return false;
+            }
+    }
+    public void notARoom(){
+        faceSouth();
+        leaveRoom();
+    }
+    public void leaveRoom(){
+        if (inside()){
+            move();
+        }
+        else{
+            faceSouth();
+            turnLeft();
+        }
+    }
+    public boolean inside(){
+        turnLeft();
+        if (frontIsClear()){
+            turnLeft();
+            turnLeft();
+            if (frontIsClear()){
+                return false;
+            }
+            else{
+                faceSouth();
+                return true;
+                
+            }
+        }
+        else{
+            faceSouth();
+            return true;
+            
+        }
+    }
+    public void faceSouth(){
+        if (facingEast()){
+            turnLeft();
+            turnLeft();
+            turnLeft();
+        }
+        if (facingWest()){
+            turnLeft();
+        }
+        if (facingNorth()){
+            turnLeft();
+            turnLeft();
+        }
     }
 }
+
 
